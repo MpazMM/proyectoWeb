b8478908f06bf4c3e81cabcf859cbaa0eae8163b
mpazMunoz <corrfeo@gmail.com>
2024-01-30 20:47:57 +0100

    Ejercicio alumno

--- ProyectoWeb/src/main/java/servlets/AlumnoServlet.java
+++ ProyectoWeb/src/main/java/servlets/AlumnoServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletConfig;
@@ -13,50 +15,59 @@ import jakarta.servlet.http.HttpServletResponse;
  * Servlet implementation class AlumnoServlet
  */
 public class AlumnoServlet extends HttpServlet {
+	
+	private List<AlumnoFormulario> alumnos = new ArrayList<AlumnoFormulario>();
+	
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public AlumnoServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public AlumnoServlet() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		String texto = config.getInitParameter("textook");
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("doPost FormularioServlet");
+		
 		String nombre = request.getParameter("nombre");
 		String apellidos = request.getParameter("apellidos");
-		String sexo = "";
-			if(request.getParameter("sexo1") != null ) {
-				sexo = "Hombre";
-			}else if(request.getParameter("sexo2") != null ) {
-				sexo = "Mujer";
-			}else {
-				sexo = "Otro";
-			}
-		String asignatura = request.getParameter("apellidos");
+		String sexo = request.getParameter("sexo");
+		String asignatura = request.getParameter("lista_de_asignaturas");
 		String areaTexto = request.getParameter("area_de_texto");
+//		response.setContentType("text/html");
+//		response.getWriter().append("<H2>Resultado: " + nombre + " " + apellidos + " " + sexo + " " 
+//				+ asignatura + " " + areaTexto + "</H2>");
+		AlumnoFormulario alumno = new AlumnoFormulario (nombre,apellidos,sexo,asignatura,areaTexto);
+		alumnos.add(alumno);
 		response.setContentType("text/html");
-		response.getWriter().append("<H2>Resultado: "+nombre+ " " + apellidos + " " + 
-				sexo + " " + asignatura + " " + areaTexto + "</H2>");
+		for (AlumnoFormulario alumnoFormulario : alumnos) {	
+			response.getWriter().append("<p>Alumno " + alumnoFormulario.getNombre() + " " + alumnoFormulario.getApellidos() + " " 
+					+alumnoFormulario.getSexo() + " " + alumnoFormulario.getAsignatura() + " " + alumnoFormulario.getAreaTexto() + "</p><br>");
+		}
+		
 		doGet(request, response);
 	}
 
