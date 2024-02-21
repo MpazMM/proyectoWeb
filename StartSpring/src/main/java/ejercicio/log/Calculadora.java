79dbef123795d0dbf6bbf3d358f70d5005aeae80
mpazMunoz <corrfeo@gmail.com>
2024-02-21 20:00:57 +0100

    Ejercicio

--- StartSpring/src/main/java/ejercicio/log/Calculadora.java
+++ StartSpring/src/main/java/ejercicio/log/Calculadora.java
@@ -1,9 +1,40 @@
 package ejercicio.log;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Calculadora {
 
+	@Qualifier("LogGetafe1")
+	@Autowired
 	private LogGetafe log;
 	private int operador1;
 	private int operador2;
+	
+	
+	public LogGetafe getLog() {
+		return log;
+	}
+	public void setLog(LogGetafe log) {
+		this.log = log;
+	}
+	public int getOperador1() {
+		return operador1;
+	}
+	public void setOperador1(int operador1) {
+		this.operador1 = operador1;
+	}
+	public int getOperador2() {
+		return operador2;
+	}
+	public void setOperador2(int operador2) {
+		this.operador2 = operador2;
+	}
+	
+	
+	
+	public void sumar(){
+	    log.pintar("El resultado es "+(operador1+operador2));
+	}	
 	
 }
--- StartSpring/src/main/java/ejercicio/log/LogGetafe.java
+++ StartSpring/src/main/java/ejercicio/log/LogGetafe.java
@@ -4,4 +4,31 @@ public class LogGetafe {
 	
 	private int nivelLog;
 
+	
+	public int getNivelLog() {
+		return nivelLog;
+	}
+
+	public void setNivelLog(int nivelLog) {
+		this.nivelLog = nivelLog;
+	}
+	
+	
+	public void pintar (String msg) {
+		if(nivelLog>=0) {
+			System.out.println("Trace. " + msg);
+		}else if(nivelLog>=1) {
+			System.out.println("Debug. " + msg);
+		}else if(nivelLog>=2) {
+			System.out.println("Info. " + msg);
+		}else if(nivelLog>=3) {
+			System.out.println("Warning. " + msg);
+		}else if(nivelLog>=4) {
+			System.out.println("Error. " + msg);
+		}else if(nivelLog>=5) {
+			System.out.println("Fatal. " + msg);
+		}
+	}
+	
+
 }
